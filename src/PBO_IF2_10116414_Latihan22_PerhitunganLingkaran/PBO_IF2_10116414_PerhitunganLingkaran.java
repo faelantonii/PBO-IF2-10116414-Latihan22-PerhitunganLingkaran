93b29aa971ab1c76931b2983ff3ae54d964acd8a
sweenutt <sweenutt@192.168.3.19>
2020-10-31 21:58:25 +0700

    Latihan 22 Program Perhitungan Lingkaran

---
+++ src/PBO_IF2_10116414_Latihan22_PerhitunganLingkaran/PBO_IF2_10116414_PerhitunganLingkaran.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PBO_IF2_10116414_Latihan22_PerhitunganLingkaran;
+
+/**
+ *
+ * @author sweenutt
+ * 
+ * NAMA         : FAEL ANTONI WIJAYA
+ * KELAS        : PBO-2
+ * NIM          : 10116414
+ * DESKRIPSI    : Program Perhitungan Lingkaran
+ */
+public class PBO_IF2_10116414_PerhitunganLingkaran {
+    public static void main(String[] args) {
+        
+    }
+    
+}
